213cb984a962fbef565652600a5bd65c423b9b39
CarloCGC <carlocgc@hotmail.com>
2018-08-07 18:35:48 +0100

    Initial Commit

---
+++ Type.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Type.Android
+{
+    [Activity(Label = "Type.Android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
