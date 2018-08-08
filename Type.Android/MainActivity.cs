657023fdad4d58498b1013d294270d640c4cdc4d
CarloCGC <carlocgc@hotmail.com>
2018-08-08 21:59:19 +0100

    Added MainActivity.cs

---
+++ Type.Android/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using AmosAndroid;
+using AmosShared.Base;
+using Android.App;
+using Android.Content.PM;
+using Android.Widget;
+using Android.OS;
+
+namespace Type.Android
+{
+    [Activity(Label = "Type.Android", MainLauncher = true)]
+    public class MainActivity : GameActivity
+    {
+        public MainActivity() : base(new Game())
+        {
+        }
+    }
+}
+
