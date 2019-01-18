744687ad95f8f722ca10dc7f75ace81c1999d2f7
CarloCGC <carlocgc@hotmail.com>
2019-01-18 18:38:27 +0000

    Created Probe and ProbeController interfaces

---
+++ Type/Interfaces/Probe/IProbe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+
+namespace Type.Interfaces.Probe
+{
+    public interface IProbe
+    {
+        Int32 ID { get; }
+
+        void Fire();
+
+        void UpdatePosition(Vector2 position);
+    }
+}
---
+++ Type/Interfaces/Probe/IProbeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+
+
+namespace Type.Interfaces.Probe
+{
+    public interface IProbeController
+    {
+        void AddProbe();
+
+        void Fire();
+
+        void RemoveAll();
+
+        void UpdatePosition(Vector2 position);
+    }
+}
