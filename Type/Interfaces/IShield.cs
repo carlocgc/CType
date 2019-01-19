8aa23d95c9e2c1b87288d07e733d8f72d5f0ef45
CarloCGC <carlocgc@hotmail.com>
2019-01-19 09:56:33 +0000

    Added Shield interface and object

---
+++ Type/Interfaces/IShield.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AmosShared.Interfaces;
+using OpenTK;
+
+namespace Type.Interfaces
+{
+    public interface IShield : IPositionable
+
+    {
+    Boolean IsActive { get; }
+
+    Vector2 Position { get; set; }
+
+    void Activate();
+
+    void Deactivate();
+    }
+}
---
+++ Type/Objects/Player/Shield.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AmosShared.Graphics.Drawables;
+using Type.Base;
+using Type.Interfaces;
+
+namespace Type.Objects.Player
+{
+    public class Shield : GameObject, IShield
+    {
+        private Sprite _Sprite;
+
+        public Boolean IsActive { get; private set; }
+
+        public Shield()
+        {
+            _Sprite = new Sprite();
+        }
+
+        public void Activate()
+        {
+            IsActive = true;
+        }
+
+        public void Deactivate()
+        {
+            
+        }
+    }
+}
