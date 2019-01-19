410b2edc3391ff3661aa005b55c3d55752748151
CarloCGC <carlocgc@hotmail.com>
2019-01-19 17:25:14 +0000

    Added stats display

--- Type/UI/StatsDisplay.cs
+++ Type/UI/StatsDisplay.cs
@@ -9,68 +9,70 @@ namespace Type.UI
 {
     public class StatsDisplay : IDisposable
     {
-        private List<TextDisplay> _Text;
+        private readonly List<TextDisplay> _Text;
 
-        private TextDisplay _ShotsFired;
+        private readonly TextDisplay _ShotsFired;
 
-        private TextDisplay _ProbesCreated;
+        private readonly TextDisplay _ProbesCreated;
 
-        private TextDisplay _ShieldsCreated;
+        private readonly TextDisplay _ShieldsCreated;
 
-        private TextDisplay _Deaths;
+        private readonly TextDisplay _Deaths;
 
-        private TextDisplay _EnemiesKilled;
+        private readonly TextDisplay _EnemiesKilled;
 
-        private TextDisplay _TimePlayed;
+        private readonly TextDisplay _TimePlayed;
 
         public StatsDisplay()
         {
+            _Text = new List<TextDisplay>();
+
             _Text.Add(_ShotsFired = new TextDisplay(Game.UiCanvas, Constants.ZOrders.UI, Texture.GetTexture("Content/Graphics/KenPixel/KenPixel.png"), Constants.Font.Map, 15, 15, "KenPixel")
             {
                 Text = $"SHOTS FIRED: {GameStats.Instance.BulletsFired}",
-                Position = new Vector2(-660, -100),
+                Position = new Vector2(-660, -200),
                 Visible = true,
-                Scale = new Vector2(3, 3),
+                Scale = new Vector2(2, 2),
                 Colour = new Vector4(1, 1, 1, 1)
             });
             _Text.Add(_ShotsFired = new TextDisplay(Game.UiCanvas, Constants.ZOrders.UI, Texture.GetTexture("Content/Graphics/KenPixel/KenPixel.png"), Constants.Font.Map, 15, 15, "KenPixel")
             {
                 Text = $"PROBES CREATED: {GameStats.Instance.ProbesCreated}",
-                Position = new Vector2(-660, -150),
+                Position = new Vector2(-660, -250),
                 Visible = true,
-                Scale = new Vector2(3, 3),
+                Scale = new Vector2(2, 2),
                 Colour = new Vector4(1, 1, 1, 1)
             });
             _Text.Add(_ShotsFired = new TextDisplay(Game.UiCanvas, Constants.ZOrders.UI, Texture.GetTexture("Content/Graphics/KenPixel/KenPixel.png"), Constants.Font.Map, 15, 15, "KenPixel")
             {
                 Text = $"SHIELDS CREATED: {GameStats.Instance.ShieldsCreated}",
-                Position = new Vector2(-660, -200),
+                Position = new Vector2(-660, -300),
                 Visible = true,
-                Scale = new Vector2(3, 3),
+                Scale = new Vector2(2, 2),
                 Colour = new Vector4(1, 1, 1, 1)
             });
             _Text.Add(_ShotsFired = new TextDisplay(Game.UiCanvas, Constants.ZOrders.UI, Texture.GetTexture("Content/Graphics/KenPixel/KenPixel.png"), Constants.Font.Map, 15, 15, "KenPixel")
             {
                 Text = $"DEATHS: {GameStats.Instance.Deaths}",
-                Position = new Vector2(200, -100),
+                Position = new Vector2(200, -200),
                 Visible = true,
-                Scale = new Vector2(3, 3),
+                Scale = new Vector2(2, 2),
                 Colour = new Vector4(1, 1, 1, 1)
             });
             _Text.Add(_ShotsFired = new TextDisplay(Game.UiCanvas, Constants.ZOrders.UI, Texture.GetTexture("Content/Graphics/KenPixel/KenPixel.png"), Constants.Font.Map, 15, 15, "KenPixel")
             {
                 Text = $"ENEMIES KILLED: {GameStats.Instance.EnemiesKilled}",
-                Position = new Vector2(200, -150),
+                Position = new Vector2(200, -250),
                 Visible = true,
-                Scale = new Vector2(3, 3),
+                Scale = new Vector2(2, 2),
                 Colour = new Vector4(1, 1, 1, 1)
             });
             _Text.Add(_ShotsFired = new TextDisplay(Game.UiCanvas, Constants.ZOrders.UI, Texture.GetTexture("Content/Graphics/KenPixel/KenPixel.png"), Constants.Font.Map, 15, 15, "KenPixel")
             {
                 Text = $"GAME TIME: {GameStats.Instance.PlayTime:hh\\:mm\\:ss}",
-                Position = new Vector2(200, -200),
+                Position = new Vector2(200, -300),
                 Visible = true,
-                Scale = new Vector2(3, 3),
+                Scale = new Vector2(2, 2),
                 Colour = new Vector4(1, 1, 1, 1)
             });
         }
