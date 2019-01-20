77e63537f05c639593baec4dbc70ec41d23bd0c3
CarloCGC <carlocgc@hotmail.com>
2019-01-20 13:51:36 +0000

    Added boss graphic and class stub

---
+++ Type/Objects/Bosses/BossA.cs
@@ -0,0 +1,27 @@
+﻿using OpenTK;
+using System;
+using Type.Objects.Enemies;
+
+namespace Type.Objects.Bosses
+{
+    public class BossA : BaseEnemy
+    {
+        /// <inheritdoc />
+        public BossA(String assetPath, Vector2 spawnPos, Single rotation, Vector2 direction, Single speed, TimeSpan fireRate, Int32 hitPoints) : base(assetPath, spawnPos, rotation, direction, speed, fireRate, hitPoints)
+        {
+            PointValue = 5000;
+        }
+
+        /// <inheritdoc />
+        protected override void Fire()
+        {
+
+        }
+
+        /// <inheritdoc />
+        public override void Update(TimeSpan timeTilUpdate)
+        {
+            base.Update(timeTilUpdate);
+        }
+    }
+}
