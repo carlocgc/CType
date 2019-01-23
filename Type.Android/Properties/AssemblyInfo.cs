46e30090cd9e17cb44763a242e8631e36495d412
CarloCGC <carlocgc@hotmail.com>
2019-01-23 20:42:02 +0000

    Fixed assembly version

--- Type.Android/Properties/AssemblyInfo.cs
+++ Type.Android/Properties/AssemblyInfo.cs
@@ -26,5 +26,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.0")]
 [assembly: AssemblyFileVersion("0.2.0")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
