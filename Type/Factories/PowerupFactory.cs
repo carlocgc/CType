9b311404f1f329e18079b9ec7abbab718f230b79
CarloCGC <carlocgc@hotmail.com>
2019-01-25 21:48:05 +0000

    Added powerup factory body

--- Type/Factories/PowerupFactory.cs
+++ Type/Factories/PowerupFactory.cs
@@ -1,26 +1,62 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 using System.Collections.Generic;
-using System.Text;
-using OpenTK;
 using Type.Interfaces;
 using Type.Interfaces.Powerups;
+using Type.Powerups;
 
 namespace Type.Factories
 {
     public class PowerupFactory : IPowerupFactory, INotifier<IPowerupFactoryListener>
     {
+        private readonly Random _Rnd = new Random(Environment.TickCount);
+
         /// <inheritdoc />
         public Boolean IsDisposed { get; set; }
 
         public PowerupFactory()
         {
-
         }
 
         /// <inheritdoc />
-        public void Create(Int32 weight, Vector2 position, Int32 amount)
+        public void Create(Int32 weight, Vector2 position)
         {
+            IPowerup powerup;
 
+            // Weight category for different distribution of powerups
+            switch (weight)
+            {
+                case 0:
+                    {
+                        //Int32 rnd = _Rnd.Next(0, 4);
+                        Int32 rnd = 0;
+
+                        switch (rnd)
+                        {
+                            case 0:
+                                {
+                                    powerup = new ExtraLifePowerup(position);
+                                    break;
+                                }
+
+                            default:
+                                {
+                                    throw new ArgumentOutOfRangeException("Powerup type does not exist");
+                                }
+                        }
+
+                        break;
+                    }
+                default:
+                    {
+                        throw new ArgumentOutOfRangeException("Powerupfactory weight category does not exist");
+                    }
+            }
+
+            foreach (IPowerupFactoryListener listener in _Listeners)
+            {
+                listener.OnPowerupCreated(powerup);
+            }
         }
 
         #region Listener
@@ -41,7 +77,6 @@ namespace Type.Factories
 
         #endregion
 
-        
         /// <inheritdoc />
         public Boolean CanUpdate()
         {
@@ -51,9 +86,7 @@ namespace Type.Factories
         /// <inheritdoc />
         public void Update(TimeSpan timeTilUpdate)
         {
-
         }
-
 
         /// <inheritdoc />
         public void Dispose()
