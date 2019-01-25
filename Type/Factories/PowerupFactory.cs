90020d7cb26a1e77a8ee94d5b4a0285bb19e2fba
CarloCGC <carlocgc@hotmail.com>
2019-01-25 21:07:50 +0000

    Added PowerupFactory.cs

---
+++ Type/Factories/PowerupFactory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using Type.Interfaces;
+using Type.Interfaces.Powerups;
+
+namespace Type.Factories
+{
+    public class PowerupFactory : IPowerupFactory, INotifier<IPowerupFactoryListener>
+    {
+        /// <inheritdoc />
+        public Boolean IsDisposed { get; set; }
+
+        public PowerupFactory()
+        {
+
+        }
+
+        /// <inheritdoc />
+        public void Create(Int32 weight, Vector2 position, Int32 amount)
+        {
+
+        }
+
+        #region Listener
+
+        private readonly List<IPowerupFactoryListener> _Listeners = new List<IPowerupFactoryListener>();
+
+        /// <inheritdoc />
+        public void RegisterListener(IPowerupFactoryListener listener)
+        {
+            if (!_Listeners.Contains(listener)) _Listeners.Add(listener);
+        }
+
+        /// <inheritdoc />
+        public void DeregisterListener(IPowerupFactoryListener listener)
+        {
+            if (_Listeners.Contains(listener)) _Listeners.Remove(listener);
+        }
+
+        #endregion
+
+        
+        /// <inheritdoc />
+        public Boolean CanUpdate()
+        {
+            return true;
+        }
+
+        /// <inheritdoc />
+        public void Update(TimeSpan timeTilUpdate)
+        {
+
+        }
+
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _Listeners.Clear();
+            IsDisposed = true;
+        }
+    }
+}
