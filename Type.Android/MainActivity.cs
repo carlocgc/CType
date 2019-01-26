62e8fd1c9b234d2ba898b9283e44c632488edb46
CarloCGC <carlocgc@hotmail.com>
2019-01-26 11:04:35 +0000

    Updated application name

--- Type.Android/MainActivity.cs
+++ Type.Android/MainActivity.cs
@@ -1,13 +1,10 @@
 ﻿using AmosAndroid;
-using AmosShared.Base;
 using Android.App;
 using Android.Content.PM;
-using Android.Widget;
-using Android.OS;
 
 namespace Type.Android
 {
-    [Activity(Label = "Type.Android", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
+    [Activity(Label = "C-Type", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : GameActivity
     {
         public MainActivity() : base(new Game())
