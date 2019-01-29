929b4da6c8eba52b3d6365a2acf3425213e6953f
CarloCGC <carlocgc@hotmail.com>
2019-01-29 11:45:00 +0000

    Added interstitial creation in MainActivity

--- Type.Android/MainActivity.cs
+++ Type.Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using AmosAndroid;
 using Android.App;
 using Android.Content.PM;
+using Android.Gms.Ads;
+using Android.OS;
 
 namespace Type.Android
 {
@@ -11,6 +13,21 @@ namespace Type.Android
         {
         }
 
+        /// <inheritdoc />
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            MobileAds.Initialize(this, "ca-app-pub-4204969324853965~4341189590"); // My Admob ID
+            MInterstitialAd = new InterstitialAd(this);
+            MInterstitialAd.AdUnitId = "ca-app-pub-3940256099942544/1033173712";                   // Test ad unit
+            LoadInterstitial();
+        }
+
+        private void LoadInterstitial()
+        {
+            AdRequest request = new AdRequest.Builder().AddTestDevice("7DBD856302197638").Build(); // Ad request
+            MInterstitialAd.LoadAd(request);
+        }
     }
 }
 
