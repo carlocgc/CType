2f52c96ea3588cf41553e9ba41ec4a894d9e7178
CarloCGC <carlocgc@hotmail.com>
2019-01-29 11:44:10 +0000

    Removed ad logic from Game.cs

--- Type/Game.cs
+++ Type/Game.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using AmosShared.Base;
+﻿using AmosShared.Base;
 using AmosShared.Graphics;
 using AmosShared.State;
-using Android.App;
-using Android.Content;
-using Android.Gms.Ads;
 using Engine.Shared.Graphics.Textures;
 using OpenTK;
 using Type.States;
-using Android.Gms.Ads;
-using Android.Icu.Text;
-using Android.Telephony;
-using Type.Ads;
 
 namespace Type
 {
@@ -22,10 +14,6 @@ namespace Type
 
         /// <summary> Main canvas for the UI elements </summary>
         public static Canvas UiCanvas;
-
-        public static InterstitialAd MInterstitialAd;
-
-        public Action OnAdClosed;
 
         public Game() : base("Test Game", 60)
         {
@@ -44,14 +32,6 @@ namespace Type
             SpritesheetLoader.LoadSheet("Content/Graphics/KenPixel/", "KenPixel.png", "KenPixel.json");
 
             StateManager.Instance.StartState(new EngineSplashState());
-
-            Context context = Android.MainActivity.Instance;
-            MInterstitialAd = new InterstitialAd(context);
-            MInterstitialAd.AdUnitId = "ca-app-pub-3940256099942544/1033173712"; // Test ad unit
-            MobileAds.Initialize(context, "ca-app-pub-4204969324853965~4341189590"); // My Admob ID
-            AdRequest request = new AdRequest.Builder().AddTestDevice("7DBD856302197638").Build(); // Ad request
-            MInterstitialAd.LoadAd(request);
-            MInterstitialAd.AdListener = new CustomAdListener();
         }
 
         protected override Vector2 CalculateExtraOffset(float heightDifference)
