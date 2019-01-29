c8dacb9cad595f5bfc5e7fffdc5be21030d64d46
CarloCGC <carlocgc@hotmail.com>
2019-01-29 11:44:30 +0000

    Added custom ad listener object

--- Type/Ads/CustomAdListener.cs
+++ Type/Ads/CustomAdListener.cs
@@ -7,28 +7,46 @@ namespace Type.Ads
 {
     public class CustomAdListener : AdListener
     {
+        public Action OnAdClosedAction { get; set; }
+
+        public Action OnAdStartedAction { get; set; }
+
+        public CustomAdListener()
+        {
+        }
+
         /// <inheritdoc />
         public override void OnAdFailedToLoad(Int32 errorCode)
         {
             base.OnAdFailedToLoad(errorCode);
-            int i = 0;
-            i++;
         }
 
         /// <inheritdoc />
         public override void OnAdLoaded()
         {
             base.OnAdLoaded();
-            Int32 i = 0;
-            i++;
+        }
+
+        /// <inheritdoc />
+        public override void OnAdOpened()
+        {
+            base.OnAdOpened();
+            OnAdStartedAction?.Invoke();
         }
 
         /// <inheritdoc />
         public override void OnAdClosed()
         {
             base.OnAdClosed();
-            Int32 i = 0;
-            i++;
+            OnAdClosedAction?.Invoke();
+        }
+
+        /// <inheritdoc />
+        protected override void Dispose(Boolean disposing)
+        {
+            base.Dispose(disposing);
+            OnAdStartedAction = null;
+            OnAdClosedAction = null;
         }
     }
 }
