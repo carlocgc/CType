5322541ede96a0d366549919ed8aa45b56cb6a71
CarloCGC <carlocgc@hotmail.com>
2019-01-31 20:57:47 +0000

    Data loader now supports new data format

--- Type/Data/LevelLoader.cs
+++ Type/Data/LevelLoader.cs
@@ -1,8 +1,8 @@
 ﻿using AmosShared.Base;
-using OpenTK;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Android.Nfc.CardEmulators;
 
 namespace Type.Data
 {
@@ -25,31 +25,52 @@ namespace Type.Data
             List<String> waveStrings = new List<String>();
             List<WaveData> _Waves = new List<WaveData>();
 
-            // Split string into wave strings
-            waveStrings = data.Split(':').ToList();
+            String cleanData = data.ToLower().Replace("\r\n", "");
 
-            // Process each wave
-            foreach (String line in waveStrings)
+            // Split data into wave strings
+            waveStrings = cleanData.Split('_').ToList();
+
+            // Temporary data store wave data object
+            List<Single> positions = new List<Single>();
+            List<Int32> types = new List<Int32>();
+            List<Single> delays = new List<Single>();
+
+            foreach (String waveString in waveStrings)
             {
-                // Split wave string into each enemy data section
-                String[] enemyStrings = line.Split(',');
-                List<Single> positions = new List<Single>();
-                List<Int32> types = new List<Int32>();
-                List<Single> delays = new List<Single>();
+                String[] waveData = waveString.Split(':');
+                String[] enemyStrings = waveData[1].Split(';');
 
-                // For each enemy in the wave
-                foreach (String es in enemyStrings)
+                foreach (String enemyString in enemyStrings)
                 {
-                    // Split enemy data section into : type, Y position, spawn delay
-                    String[] shipdata = es.Split('_');
-                    Int32.TryParse(shipdata[0], out Int32 shiptype);
-                    Single.TryParse(shipdata[1], out Single yPos);
-                    Single.TryParse(shipdata[2], out Single delay);
+                    String[] enemieParts = enemyString.Split('|');
 
-                    // Add each parsed string to its respective list
-                    types.Add(shiptype);
-                    positions.Add(yPos);
-                    delays.Add(delay);
+                    foreach (String part in enemieParts)
+                    {
+                        String[] partSplit = part.Split('=');
+
+                        switch (partSplit[0])
+                        {
+                            case "type":
+                                {
+                                    types.Add(Int32.Parse(partSplit[1]));
+                                    break;
+                                }
+                            case "ypos":
+                                {
+                                    positions.Add(Int32.Parse(partSplit[1]));
+                                    break;
+                                }
+                            case "delay":
+                                {
+                                    delays.Add(Single.Parse(partSplit[1]));
+                                    break;
+                                }
+                            default:
+                                {
+                                    throw new ArgumentOutOfRangeException("Enemy component does not exist");
+                                }
+                        }
+                    }
                 }
 
                 // Create the delay time spans
@@ -61,6 +82,10 @@ namespace Type.Data
 
                 // Create the wave data
                 _Waves.Add(new WaveData(delaySpans.ToArray(), types.ToArray(), positions.ToArray()));
+
+                delaySpans.Clear();
+                types.Clear();
+                positions.Clear();
             }
 
             return _Waves;
