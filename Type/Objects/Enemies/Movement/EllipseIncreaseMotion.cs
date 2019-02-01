69c892ee711469b22b63e3945f161d0d3b1ea9b4
CarloCGC <carlocgc@hotmail.com>
2019-02-01 17:22:51 +0000

    Set max level to 3

---
+++ Type/Objects/Enemies/Movement/EllipseIncreaseMotion.cs
@@ -0,0 +1,36 @@
+﻿using OpenTK;
+using System;
+using Type.Interfaces.Movement;
+
+namespace Type.Objects.Enemies.Movement
+{
+    /// <summary>
+    /// Creates movement in an elipse while increasing Y axis
+    /// </summary>
+    public class EllipseIncreaseMotion : IAccelerationProvider
+    {
+        /// <summary> Movement speed </summary>
+        private readonly Single _Speed;
+        /// <summary> Movement direction </summary>
+        private Vector2 _Direction;
+
+        public EllipseIncreaseMotion(Vector2 direction, Single speed)
+        {
+            _Direction = direction;
+            _Speed = speed;
+        }
+
+        /// <summary>
+        /// Updates a given vector using a modifier
+        /// </summary>
+        /// <param name="timeTilUpdate"></param>
+        /// <returns> The modifed vector </returns>
+        public Vector2 ApplyAcceleration(Vector2 baseVector, TimeSpan timeTilUpdate)
+        {
+            baseVector.Y += _Speed * (Single) timeTilUpdate.TotalSeconds;
+            baseVector.X = 1f/200f * (baseVector.Y * baseVector.Y);
+
+            return baseVector;
+        }
+    }
+}
--- Type/States/PlayingState.cs
+++ Type/States/PlayingState.cs
@@ -19,7 +19,7 @@ namespace Type.States
     public class PlayingState : State, IPlayerListener, IEnemyListener, IEnemyFactoryListener, IPowerupListener, IPowerupFactoryListener
     {
         /// <summary> Max level of the game </summary>
-        private readonly Int32 _MaxLevel = 2;
+        private readonly Int32 _MaxLevel = 3;
         /// <summary> THe type of player craft </summary>
         private readonly Int32 _PlayerType;
 
@@ -53,7 +53,7 @@ namespace Type.States
 
         protected override void OnEnter()
         {
-            _CurrentLevel = 2;
+            _CurrentLevel = 1;
 
             _EnemyFactory = new EnemyFactory();
             _EnemyFactory.RegisterListener(this);
