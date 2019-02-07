d080a5834292a0958c8ec283146a519511fba8ec
CarloCGC <carlocgc@hotmail.com>
2019-02-07 22:47:15 +0000

    Added pause and resume

---
+++ Type/Interfaces/Control/IResumeButtonListener.cs
@@ -0,0 +1,12 @@
+﻿namespace Type.Interfaces.Control
+{
+    /// <summary> Interface for objects that listen to the resume button </summary>
+    public interface IResumeButtonListener
+    {
+        /// <summary> Invoked when the pause button is pressed </summary>
+        void OnResumePressed();
+
+        /// <summary> Invoked when the pause button is released </summary>
+        void OnResumeReleased();
+    }
+}
