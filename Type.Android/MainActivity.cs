6979007620fa180d9ed8a5f5da8775ec6fc65a5a
CarloCGC <carlocgc@hotmail.com>
2019-02-09 10:04:27 +0000

    Added back press override to avoid it restarting the main activity

--- Type.Android/MainActivity.cs
+++ Type.Android/MainActivity.cs
@@ -4,12 +4,23 @@ using Android.Content.PM;
 
 namespace Type.Android
 {
-    [Activity(Label = "C:Type", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
+    [Activity(Label = "C:Type", Name = "com.ctype.MainActivity", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : GameActivity
     {
         public MainActivity() : base(new Game())
         {
         }
+
+        #region Overrides of Activity
+
+        /// <summary>
+        /// Invoked when the user presses the back button, do not call base as no functionality is required from the press
+        /// </summary>
+        public override void OnBackPressed()
+        {
+        }
+
+        #endregion
     }
 }
 
