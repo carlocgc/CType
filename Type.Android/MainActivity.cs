8afea032027668abf8085617835d73e5c1636f10
CarloCGC <carlocgc@hotmail.com>
2019-09-16 18:47:11 +0100

    WIP firebase

--- Type.Android/MainActivity.cs
+++ Type.Android/MainActivity.cs
@@ -1,15 +1,38 @@
 ﻿using AmosAndroid;
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Firebase.Analytics;
 
 namespace Type.Android
 {
     [Activity(Label = "C:Type", Name = "com.ctype.MainActivity", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : GameActivity
     {
+
+        private FirebaseAnalytics _FirebaseAnalytics;
+
         public MainActivity() : base(new Game())
         {
         }
+
+
+        #region Overrides of GameActivity
+
+        /// <summary> Called when the activity is created - it creates the view and the game </summary>
+        /// <param name="bundle"></param>
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            _FirebaseAnalytics = FirebaseAnalytics.GetInstance(this);
+            Bundle eventBundle = new Bundle();
+            eventBundle.PutString(FirebaseAnalytics.Param.ItemId, "TestID");
+            eventBundle.PutString(FirebaseAnalytics.Param.ItemName, "GameLaunch");
+            eventBundle.PutString(FirebaseAnalytics.Param.ContentType, "TestType");
+            _FirebaseAnalytics.LogEvent(FirebaseAnalytics.Event.SelectContent, eventBundle);
+        }
+
+        #endregion
 
         #region Overrides of Activity
 
